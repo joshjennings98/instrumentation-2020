c5a2a2f7ca469dfc7f527973864e0f1b2712d712
Josh <jenninjl01@gmail.com>
2020-01-26 15:30:04 +0000

    Initial commit. Has winforms thing and basic skeleton

---
+++ Instrumentation2020/ComplexImpedanceAnalyser.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Instrumentation2020
+{
+    partial class ComplexImpedanceAnalyser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbPortSettings = new System.Windows.Forms.GroupBox();
+            this.timeoutBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbPortName = new System.Windows.Forms.ComboBox();
+            this.cmbBaudRate = new System.Windows.Forms.ComboBox();
+            this.lblComPort = new System.Windows.Forms.Label();
+            this.lblBaudRate = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.rtfTerminal = new System.Windows.Forms.RichTextBox();
+            this.Measure = new System.Windows.Forms.Button();
+            this.resetbutton = new System.Windows.Forms.Button();
+            this.gbPortSettings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbPortSettings
+            // 
+            this.gbPortSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.gbPortSettings.Controls.Add(this.timeoutBox);
+            this.gbPortSettings.Controls.Add(this.label1);
+            this.gbPortSettings.Controls.Add(this.cmbPortName);
+            this.gbPortSettings.Controls.Add(this.cmbBaudRate);
+            this.gbPortSettings.Controls.Add(this.lblComPort);
+            this.gbPortSettings.Controls.Add(this.lblBaudRate);
+            this.gbPortSettings.Location = new System.Drawing.Point(12, 175);
+            this.gbPortSettings.Name = "gbPortSettings";
+            this.gbPortSettings.Size = new System.Drawing.Size(242, 64);
+            this.gbPortSettings.TabIndex = 5;
+            this.gbPortSettings.TabStop = false;
+            this.gbPortSettings.Text = "COM Serial Port Settings";
+            // 
+            // timeoutBox
+            // 
+            this.timeoutBox.DropDownWidth = 67;
+            this.timeoutBox.FormattingEnabled = true;
+            this.timeoutBox.ItemHeight = 13;
+            this.timeoutBox.Items.AddRange(new object[] {
+            "1000",
+            "2000",
+            "5000",
+            "7000",
+            "10000"});
+            this.timeoutBox.Location = new System.Drawing.Point(161, 35);
+            this.timeoutBox.Name = "timeoutBox";
+            this.timeoutBox.Size = new System.Drawing.Size(69, 21);
+            this.timeoutBox.TabIndex = 5;
+            this.timeoutBox.SelectedIndexChanged += new System.EventHandler(this.timeoutBox_SelectedIndexChanged);
+            this.timeoutBox.TextUpdate += new System.EventHandler(this.timeoutBox_TextUpdate);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Timeout (ms):";
+            // 
+            // cmbPortName
+            // 
+            this.cmbPortName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPortName.FormattingEnabled = true;
+            this.cmbPortName.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6"});
+            this.cmbPortName.Location = new System.Drawing.Point(13, 35);
+            this.cmbPortName.Name = "cmbPortName";
+            this.cmbPortName.Size = new System.Drawing.Size(67, 21);
+            this.cmbPortName.TabIndex = 1;
+            this.cmbPortName.SelectedIndexChanged += new System.EventHandler(this.cmbPortName_SelectedIndexChanged);
+            // 
+            // cmbBaudRate
+            // 
+            this.cmbBaudRate.DropDownWidth = 67;
+            this.cmbBaudRate.FormattingEnabled = true;
+            this.cmbBaudRate.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.cmbBaudRate.Location = new System.Drawing.Point(86, 35);
+            this.cmbBaudRate.Name = "cmbBaudRate";
+            this.cmbBaudRate.Size = new System.Drawing.Size(69, 21);
+            this.cmbBaudRate.TabIndex = 3;
+            this.cmbBaudRate.SelectedIndexChanged += new System.EventHandler(this.cmbBaudRate_SelectedIndexChanged);
+            this.cmbBaudRate.TextUpdate += new System.EventHandler(this.cmbBaudRate_TextUpdate);
+            // 
+            // lblComPort
+            // 
+            this.lblComPort.AutoSize = true;
+            this.lblComPort.Location = new System.Drawing.Point(12, 19);
+            this.lblComPort.Name = "lblComPort";
+            this.lblComPort.Size = new System.Drawing.Size(56, 13);
+            this.lblComPort.TabIndex = 0;
+            this.lblComPort.Text = "COM Port:";
+            // 
+            // lblBaudRate
+            // 
+            this.lblBaudRate.AutoSize = true;
+            this.lblBaudRate.Location = new System.Drawing.Point(85, 19);
+            this.lblBaudRate.Name = "lblBaudRate";
+            this.lblBaudRate.Size = new System.Drawing.Size(61, 13);
+            this.lblBaudRate.TabIndex = 2;
+            this.lblBaudRate.Text = "Baud Rate:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(264, 164);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // rtfTerminal
+            // 
+            this.rtfTerminal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtfTerminal.Location = new System.Drawing.Point(12, 12);
+            this.rtfTerminal.Name = "rtfTerminal";
+            this.rtfTerminal.Size = new System.Drawing.Size(330, 146);
+            this.rtfTerminal.TabIndex = 11;
+            this.rtfTerminal.Text = "";
+            this.rtfTerminal.TextChanged += new System.EventHandler(this.rtfTerminal_TextChanged);
+            // 
+            // Measure
+            // 
+            this.Measure.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Measure.Location = new System.Drawing.Point(264, 222);
+            this.Measure.Name = "Measure";
+            this.Measure.Size = new System.Drawing.Size(75, 23);
+            this.Measure.TabIndex = 12;
+            this.Measure.Text = "&Measure";
+            this.Measure.UseVisualStyleBackColor = true;
+            this.Measure.Click += new System.EventHandler(this.Measure_Click);
+            // 
+            // resetbutton
+            // 
+            this.resetbutton.Location = new System.Drawing.Point(264, 193);
+            this.resetbutton.Name = "resetbutton";
+            this.resetbutton.Size = new System.Drawing.Size(75, 23);
+            this.resetbutton.TabIndex = 13;
+            this.resetbutton.Text = "Reset";
+            this.resetbutton.UseVisualStyleBackColor = true;
+            this.resetbutton.Click += new System.EventHandler(this.resetbutton_Click);
+            // 
+            // ComplexImpedanceAnalyser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 251);
+            this.Controls.Add(this.resetbutton);
+            this.Controls.Add(this.Measure);
+            this.Controls.Add(this.rtfTerminal);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.gbPortSettings);
+            this.Name = "ComplexImpedanceAnalyser";
+            this.Text = "Complex Impedance Analyser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbPortSettings.ResumeLayout(false);
+            this.gbPortSettings.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox gbPortSettings;
+        private System.Windows.Forms.ComboBox cmbPortName;
+        private System.Windows.Forms.ComboBox cmbBaudRate;
+        private System.Windows.Forms.Label lblComPort;
+        private System.Windows.Forms.Label lblBaudRate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox rtfTerminal;
+        private System.Windows.Forms.Button Measure;
+        private System.Windows.Forms.ComboBox timeoutBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button resetbutton;
+    }
+}
+
---
+++ Instrumentation2020/ComplexImpedanceAnalyser.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Instrumentation2020
+{
+    public partial class ComplexImpedanceAnalyser : Form
+    {
+        static SerialPort _serialPort;
+        int readTimeout = 1000;
+
+        public class Message
+        {
+            public int ID;
+            private string dataType;
+            private string data;
+            public string prettyVersion;
+
+            public Message(string message)
+            {
+                this.ID = Int32.Parse(message.Substring(2, 2));
+                dataType = this.getDataType(message.Substring(4, 2));
+                data = this.getData(message.Substring(6, 5), dataType);
+                this.prettyVersion = String.Concat("The ", dataType, " is ", data, ".\n");
+            }
+            private string getDataType(string data)
+            {
+                string dataType = "NULL"; // get rid of once I sort the exception case
+
+                switch (data)
+                {
+                    case "00":
+                        dataType = "Magnitude";
+                        break;
+                    case "01":
+                        dataType = "Phase";
+                        break;
+                    // Add the other cases
+                    default:
+                        Console.WriteLine("This shouldn't happen, add an exception here or something.");
+                        break;
+                }
+                return dataType;
+            }
+
+            private string getData(string data, string dataType)
+            {
+                string newData = data;
+
+                switch (dataType)
+                {
+                    case "Magnitude":
+                        newData = String.Concat(data, "Ω");
+                        break;
+                    case "Phase":
+                        dataType = String.Concat(data, "⌀");
+                        break;
+                    // Add the other cases
+                    default:
+                        Console.WriteLine("This shouldn't happen, add another exception here or something.");
+                        break;
+                }
+                return newData;
+            }
+        }
+
+        public ComplexImpedanceAnalyser()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clear and scroll back to top
+            rtfTerminal.Clear();
+            rtfTerminal.SelectionStart = 0;
+            rtfTerminal.ScrollToCaret();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Set initial indexes (and clear display so no clutter)
+            cmbPortName.SelectedIndex = 0;
+            cmbBaudRate.SelectedIndex = 0;
+            timeoutBox.SelectedIndex = 0;
+            rtfTerminal.Clear();
+
+            // Get list of ports available and put into list
+            List<string> ports = new List<string>();
+            foreach (string s in SerialPort.GetPortNames()) {
+                ports.Add(s);
+            };
+
+            // Set data source
+            cmbPortName.DataSource = ports;
+
+            // Create a new SerialPort object with default settings.
+            _serialPort = new SerialPort();            
+        }
+
+        private void changePort()
+        {
+            Console.WriteLine(cmbBaudRate);
+            // Make a new serial port object with the new baud rate
+            if (cmbBaudRate.Text != "") {
+                _serialPort = new SerialPort(cmbPortName.Text, Int32.Parse(cmbBaudRate.Text));
+            }
+            else
+            {
+                _serialPort = new SerialPort(cmbPortName.Text);
+            }
+            rtfTerminal.Text += "Set port to " + _serialPort.PortName + " with a baud rate of " + _serialPort.BaudRate.ToString() + ".\n";
+        }
+
+        private void cmbPortName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            changePort();
+        }
+        
+        private void cmbBaudRate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            changePort();
+        }
+        private void cmbBaudRate_TextUpdate(object sender, EventArgs e)
+        {
+            changePort();
+        }
+
+        private void rtfTerminal_TextChanged(object sender, EventArgs e)
+        {
+            // Auto scroll to bottom
+            rtfTerminal.SelectionStart = rtfTerminal.Text.Length;
+            rtfTerminal.ScrollToCaret();
+        }
+
+        private string readSerial(SerialPort port)
+        {
+            string data = "";
+            int numParams = 8;
+            var done = new Dictionary<int, Message>();
+
+            try
+            {
+                // Assuming data is a 12 byte string |FF|id|datatype|data|checksum|                
+                // Want all the information, so repeat until you have everything
+                while (done.Count < numParams)
+                {
+                    string serialData = port.ReadLine();
+                    if (true) { // need to make this a thing that checks the checksum to make sure it is valid
+                        // Convert into message form
+                        Message msg = new Message(serialData);
+                        
+                        // Only add it to map if it isn't already in there
+                        if (!done.ContainsKey(msg.ID)) {
+                            done.Add(msg.ID, msg);
+                        }
+                    }
+                }
+
+                // Make it pretty
+                foreach (KeyValuePair<int, Message> message in done)
+                {
+                    data += message.Value.prettyVersion;
+                }
+
+                return data;
+            }
+            catch (TimeoutException)
+            {
+                data = _serialPort.PortName + " timed out.";
+            }
+            return data;
+        }
+
+        private void Measure_Click(object sender, EventArgs e)
+        {
+            // Set the timeout and open the port
+            _serialPort.ReadTimeout = readTimeout;
+            _serialPort.Open();
+
+            // Try and read the serial port data
+            string message = readSerial(_serialPort);
+
+            // Write the data and close the port
+            rtfTerminal.Text += message + "\n";
+            _serialPort.Close();
+        }
+
+        private void changeTimeOut()
+        {
+            // Read and set timeout data
+            readTimeout = Int32.Parse(timeoutBox.Text);
+            rtfTerminal.Text += "Port timeout set to " + readTimeout + "ms.\n";
+        }
+
+        private void timeoutBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            changeTimeOut();
+        }
+
+        private void timeoutBox_TextUpdate(object sender, EventArgs e)
+        {
+            changeTimeOut();
+        }
+
+        private void resetbutton_Click(object sender, EventArgs e)
+        {
+            // Reset everything
+            cmbPortName.SelectedIndex = 0;
+            cmbBaudRate.SelectedIndex = 0;
+            timeoutBox.SelectedIndex = 0;
+            rtfTerminal.Clear();
+            rtfTerminal.Text += "Settings reset to defaults.\n";
+        }
+    }
+}
